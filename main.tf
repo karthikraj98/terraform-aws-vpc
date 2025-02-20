1fccc50df87c846607a8b8b8cdb1d8f6d3e37ce5
karthikraj98 <karthikbayanna98@gmail.com>
2025-02-20 18:27:53 +0530

    commit changes

--- main.tf
+++ main.tf
@@ -140,19 +140,19 @@ resource "aws_route_table" "database" {
 resource "aws_route" "public" {
   route_table_id            = aws_route_table.public.id
   destination_cidr_block    = "0.0.0.0/0"
-  gateway_id = "aws_internet_gateway_main.id"
+  gateway_id = aws_internet_gateway.main.id
 }
 
 resource "aws_route" "private_nat" {
   route_table_id            = aws_route_table.private.id
   destination_cidr_block    = "0.0.0.0/0"
-  nat_gateway_id = "aws_nat_gateway_main.id"
+  nat_gateway_id = aws_nat_gateway.main.id
 }
 
 resource "aws_route" "database_nat" {
   route_table_id            = aws_route_table.database.id
   destination_cidr_block    = "0.0.0.0/0"
-  nat_gateway_id = "aws_nat_gateway_main.id"
+  nat_gateway_id = aws_nat_gateway.main.id
 }
 
 resource "aws_route_table_association" "public" {
