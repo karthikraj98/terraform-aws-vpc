4a0173924b81689c35f91046f30a5838e0ff8770
karthikraj98 <karthikbayanna98@gmail.com>
2025-02-23 16:25:52 +0530

    commit changes

--- main.tf
+++ main.tf
@@ -1,16 +1,3 @@
-# resource "aws_vpc" "main" {
-#   cidr_block       = var.vpc_cidr
-#   enable_dns_hostnames = var.enable_dns_hostnames
-
-#   tags = merge(
-#     var.common_tags,
-#     var.vpc_tags,
-#     {
-#         Name = local.resource_name
-#     }
-#   )
-# }
-
 resource "aws_vpc" "main" {
   cidr_block       = var.vpc_cidr
   enable_dns_hostnames = var.enable_dns_hostnames
